41e7126d7a8c3d6d875b16a7957f7b6e3ed4834a
Bradley <32890981+Brad123ghost@users.noreply.github.com>
2023-08-29 20:58:23 +1200

    updated files

--- catalogue-service/catalogue.py
+++ catalogue-service/catalogue.py
@@ -11,17 +11,20 @@ DB_HOST = os.environ["DB_HOST"]
 app = Flask(__name__)
 
 def init_tables():
-    CREATE_TABLE = "CREATE TABLE IF NOT EXISTS CATALOGUE (\
+    CREATE_CATALOGUE_TABLE = "CREATE TABLE IF NOT EXISTS CATALOGUE (\
                         productid serial PRIMARY KEY,\
                         productcode TEXT NOT NULL,\
                         productname TEXT NOT NULL,\
                         productprice MONEY NOT NULL,\
                         productdesc TEXT,\
-                        productimage TEXT\
-                    )"
+                        productimage TEXT)"
+
+    CREATE_CATEGORY_TABLE = "CREATE TABLE IF NOT EXISTS CATEGORY (\
+                        categoryid serial PRIMARY KEY,\
+                        categoryname TEXT NOT NULL)"
     conn = psycopg2.connect(host=DB_HOST, database=DB, user=DB_USER, password=DB_PASSWORD)
     cursor = conn.cursor()
-    cursor.execute(CREATE_TABLE)
+    cursor.executemany(CREATE_CATALOGUE_TABLE, CREATE_CATEGORY_TABLE)
     conn.commit()
     cursor.close()
     conn.close()
@@ -29,14 +32,15 @@ def init_tables():
 def insert_data():
     SQL = "INSERT INTO CATALOGUE (productcode, productname, productprice, productdesc, productimage) VALUES (%s, %s, %s, %s, %s)"
     DATA = [
-        ('ab-dragon-emb-hoodie-white-front-10005040', 'Dragon Embroidered Box Fit Hoodie', 59.99,'Introducing our Oversize Dragon Hoodie, a fusion of modern streetwear and traditional Japanese aesthetics. This unique hoodie features embroidered dragon illustrations on both arms with Japanese symbols on the front and back. The box fit, slouchy shoulders, and hood offer a relaxed yet laid back look. Style it with a puffer vest to showcase the arm illustrations. Embrace the boldness of the dragon\'s symbolism and make a statement wherever you go. Elevate your streetwear game with this eye-catching hoodie.', '../static/images/ab-dragon-emb-hoodie-white-front-10005040.webp'),
-        ('bd-photosynthesis-hoodie-snow-marle-front-10004945', 'Photosythnesis Graphic Print Hoodie', 59.99, 'Introducing our Photosynthesis Hoodie, crafted from a soft cotton blend for a comfortable composition. Our in-house designed graphics take inspiration from nature, creating a captivating visual experience. Whether you\'re a nature enthusiast or not, these graphics are not to be missed. Elevate your style with this winning hoodie choice that combines comfort, quality, and eye-catching design.', '../static/images/bd-photosynthesis-hoodie-snow-marle-front-10004945.webp'),
-        ('ab-butterfly-emb-box-hooded-sweat-tan-front-10004429', 'Butterfly Embroidered Box Fit Hoodie', 59.99, 'The Butterfly Emb Box Hooded Sweat. We are bringing back our box fit hoodies in style. Supporting a loose-fitting body with slouchy shoulders. Featuring a butterfly embroidery on the front for that extra bit of detail. An easy way to add some style into your daily looks. Woven from cotton blend fabric for a cosy feel.', '../static/images/ab-butterfly-emb-box-hooded-sweat-tan-front-10004429.webp'),
-        ('ab-yoga-retreat-crew-green-front-10004852', 'Yoga Retreat Crew Neck Sweat', 49.99, 'Introducing the Yoga Retreat Crew Neck Sweat, a minimalist design that adds versatility and comfort to your wardrobe. This sweatshirt features drop shoulders and an oversized fit for a relaxed and laid-back style. Made from 100% cotton, it provides both comfort and durability. Wear it on your daily walks or pair it with jeans or shorts for a casual, effortless look. Experience the perfect blend of quality comfort and style with the Yoga Retreat Crew Neck Sweat.', '../static/images/ab-yoga-retreat-crew-green-front-10004852.webp'),
-        ('ab-ancient-alien-sweat-solid-black-front-10004848', 'Ancient Alien Graphic Print Sweat', 49.99, 'Introducing the Urban Graphic Sweat - with a crew neck, drop shoulder design, and ribbed neckband, this sweatshirt offers both comfort and a fashionable edge. <br><br>Made for those who appreciate urban fashion and unique details, the Urban Graphic Sweat combines comfort with a touch of luxury. Its metallic gold symbols give it an elevated finish, ensuring you stand out from the crowd.', '../static/images/ab-ancient-alien-sweat-solid-black-front-10004848.webp')
+        ('ab-dragon-emb-hoodie-white-front-10005040', 'Dragon Embroidered Box Fit Hoodie', 59.99,'Introducing our Oversize Dragon Hoodie, a fusion of modern streetwear and traditional Japanese aesthetics. This unique hoodie features embroidered dragon illustrations on both arms with Japanese symbols on the front and back. The box fit, slouchy shoulders, and hood offer a relaxed yet laid back look. Style it with a puffer vest to showcase the arm illustrations. Embrace the boldness of the dragon\'s symbolism and make a statement wherever you go. Elevate your streetwear game with this eye-catching hoodie.', '../static/images/products/hoodies/ab-dragon-emb-hoodie-white-front-10005040.webp'),
+        ('bd-photosynthesis-hoodie-snow-marle-front-10004945', 'Photosythnesis Graphic Print Hoodie', 59.99, 'Introducing our Photosynthesis Hoodie, crafted from a soft cotton blend for a comfortable composition. Our in-house designed graphics take inspiration from nature, creating a captivating visual experience. Whether you\'re a nature enthusiast or not, these graphics are not to be missed. Elevate your style with this winning hoodie choice that combines comfort, quality, and eye-catching design.', '../static/images/products/hoodies/bd-photosynthesis-hoodie-snow-marle-front-10004945.webp'),
+        ('ab-butterfly-emb-box-hooded-sweat-tan-front-10004429', 'Butterfly Embroidered Box Fit Hoodie', 59.99, 'The Butterfly Emb Box Hooded Sweat. We are bringing back our box fit hoodies in style. Supporting a loose-fitting body with slouchy shoulders. Featuring a butterfly embroidery on the front for that extra bit of detail. An easy way to add some style into your daily looks. Woven from cotton blend fabric for a cosy feel.', '../static/images/products/hoodies/ab-butterfly-emb-box-hooded-sweat-tan-front-10004429.webp'),
+        ('ab-yoga-retreat-crew-green-front-10004852', 'Yoga Retreat Crew Neck Sweat', 49.99, 'Introducing the Yoga Retreat Crew Neck Sweat, a minimalist design that adds versatility and comfort to your wardrobe. This sweatshirt features drop shoulders and an oversized fit for a relaxed and laid-back style. Made from 100\% cotton, it provides both comfort and durability. Wear it on your daily walks or pair it with jeans or shorts for a casual, effortless look. Experience the perfect blend of quality comfort and style with the Yoga Retreat Crew Neck Sweat.', '../static/images/products/hoodies/ab-yoga-retreat-crew-green-front-10004852.webp'),
+        ('ab-ancient-alien-sweat-solid-black-front-10004848', 'Ancient Alien Graphic Print Sweat', 49.99, 'Introducing the Urban Graphic Sweat - with a crew neck, drop shoulder design, and ribbed neckband, this sweatshirt offers both comfort and a fashionable edge. <br><br>Made for those who appreciate urban fashion and unique details, the Urban Graphic Sweat combines comfort with a touch of luxury. Its metallic gold symbols give it an elevated finish, ensuring you stand out from the crowd.', '../static/images/products/hoodies/ab-ancient-alien-sweat-solid-black-front-10004848.webp')
     ]
 
     conn = psycopg2.connect(host=DB_HOST, database=DB, user=DB_USER, password=DB_PASSWORD)
+
     cursor = conn.cursor()
     cursor.executemany(SQL, DATA)
     conn.commit()
@@ -54,7 +58,7 @@ def load_catalogue():
     results = cursor.fetchall()
     
     for row in results:
-        products.append((row[0], row[1], row[2], row[3], row[4], row[5]))
+        products.append((row[1], row[2], row[3], row[4], row[5]))
     cursor.close()
     conn.close()
     
@@ -67,7 +71,32 @@ def listcatalogue():
     
     return {"success":True, "list":catalogue}
 
+def get_product_details(productcode):
+    products = []
+    # products.append(productcode)
+    LOAD_CATALGOUE = "SELECT * FROM CATALOGUE WHERE productcode=%s"
+    PRODUCT_CODE = productcode
+    conn = psycopg2.connect(host=DB_HOST, database=DB, user=DB_USER, password=DB_PASSWORD)
+    cursor = conn.cursor()
+    cursor.execute(LOAD_CATALGOUE, [productcode])
+    results = cursor.fetchall()
+    
+    for row in results:
+        products.append((row[1], row[2], row[3], row[4], row[5]))
+    cursor.close()
+    conn.close()
+    
+    return products
+
+@app.route("/catalogue/product/<productcode>", methods=["POST", "GET"])
+def displayproduct(productcode):
+
+    product_details = get_product_details(productcode)
+
+    return {"success":True, "list":product_details}
+
 time.sleep(12)
 init_tables()
 insert_data()
 app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
+# app.run(host="0.0.0.0", port=5000)
--- frontend-service/app.py
+++ frontend-service/app.py
@@ -1,12 +1,19 @@
-from flask import Flask, request, render_template, redirect, Response
+from flask import Flask, request, render_template, redirect, Response, url_for
 import requests
 import os
 
 class Product(object):
+    # def __init__(self, productcode, productname, productprice, productdesc, productimage):
+    #     self.productcode = productcode
+    #     self.productname = productname
+    #     self.productprice = productprice
+    #     self.productdesc = productdesc
+    #     self.productimage = productimage
+
     def __init__(self, productcode, productname, productprice, productdesc, productimage):
         self.productcode = productcode
         self.productname = productname
-        self.productprice = productprice
+        self.productimage = productimage
         self.productdesc = productdesc
         self.productimage = productimage
 
@@ -32,7 +39,7 @@ def catalogue():
     productslist = []
     
     for products in res["list"]:
-        productslist.append(Product(products[1],products[2],products[3],products[4],products[5]))
+        productslist.append(Product(products[0],products[1],products[2],products[3],products[4]))
     
     return render_template("catalogue.html", products=productslist)
 
@@ -56,4 +63,21 @@ def dashboard():
 @app.route("/inventory", methods=["GET"])
 def inventory():
     return render_template("inventory.html")
+
+@app.route("/product/<productcode>", methods=["GET", "POST"])
+def view_product(productcode):
+    # res = requests.post("http://catalogueservice:5000/catalogue/list").json()
+    res = requests.post("http://catalogueservice:5000/catalogue/product/"+str(productcode)).json()
+    # res = url_for("http://catalogueservice/displayproduct", productcode=productcode).json()
+    product_details = []
+
+    for product in res["list"]:
+        product_details.append(Product(product[0],product[1],product[2],product[3],product[4]))
+
+    return render_template("product.html", product_details=product_details)
+
+@app.route("/inventory/addnewproduct", methods=["POST"])
+def addnewproduct():
+    return render_template("addnewproduct.html")
 app.run(host="0.0.0.0", port=5000, debug=True)
+# app.run(host="0.0.0.0", port=5000)
